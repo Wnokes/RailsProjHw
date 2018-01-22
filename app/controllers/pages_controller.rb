d9f1609064061326dc92073f0598d998f97a4ec6
Wnokes <westonroynokes@gmail.com>
2018-01-22 10:10:19 -0700

    Added home about contact thebestPageEver and alrightPage

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def theBestPageEver
+  end
+
+  def alrightPage
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/theBestPageEver'
+
+  get 'pages/alrightPage'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
